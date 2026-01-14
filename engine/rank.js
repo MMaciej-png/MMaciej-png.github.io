76623b67d4fd5346c9184176a4bef64767c0455a
Maciej <maciej04k@gmail.com>
2026-01-14 01:52:05 +0000

    changed rank names to match image names

--- engine/rank.js
+++ engine/rank.js
@@ -1,10 +1,10 @@
 export const RANKS = [
-  "bronze",
-  "silver",
-  "gold",
-  "platinum",
-  "diamond",
-  "master"
+  "BRONZE",
+  "SILVER",
+  "GOLD",
+  "PLATINUM",
+  "DIAMOND",
+  "MASTER"
 ];
 
 export const RANK_STEP = 200;
@@ -13,7 +13,7 @@ export function calculateRank({ totalPoints, totalItems }) {
   const basePoints = totalItems * 100;
   const delta = totalPoints - basePoints;
 
-  const silverIndex = RANKS.indexOf("silver");
+  const silverIndex = RANKS.indexOf("SILVER");
   const offset = Math.floor(delta / RANK_STEP);
   
   const index = Math.max(
