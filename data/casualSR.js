b5e56d3ae6a974e39056943015ea2007ea91a5e0
Maciej <maciej04k@gmail.com>
2026-01-30 14:19:22 +0000

    sr fix

--- data/casualSR.js
+++ data/casualSR.js
@@ -4,12 +4,22 @@ import { calculateRank } from "../engine/rank.js";
 export async function getCasualSR() {
   const items = await loadItems();
 
-  const totalPoints = items.reduce(
-    (sum, i) => sum + i.points,0
-  );
+  const seen = new Map();
+
+  for (const i of items) {
+    const key = `${i.type}::${i.indo}::${i.eng}`;
+
+    // Only count the concept once
+    if (!seen.has(key)) {
+      seen.set(key, i.points);
+    }
+  }
+
+  const totalPoints = [...seen.values()]
+    .reduce((sum, p) => sum + p, 0);
 
   return calculateRank({
     totalPoints,
-    totalItems: items.length
+    totalItems: seen.size
   });
 }
